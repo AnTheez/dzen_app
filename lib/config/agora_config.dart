bf6df8d4a3afcd31abf146077a3d82ec62344e15
Aiter <vsimvs.17879@gmail.com>
2023-05-14 09:46:52 +0300

    v 1.0.1

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,7 @@
+import 'package:dzen_app/constants.dart';
+
+class AgoraConfig {
+  static String token = '';
+  static String appId = agoraappid;
+  static String appcertificate = agoraappcertificate;
+}
---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const String agoraappid = '7d3df7fc34254fa688a4432535c95aae';
+const String agoraappcertificate = '28f692984c334de0ad20d71e1b48315a';
+const String baseUrl = 'https://dzen.herokuapp.com/';
